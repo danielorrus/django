09e4f511894179f53abd7588aaf5256e7ee87218
danielorrus <drgponte@gmail.com>
2022-02-27 16:42:48 +0100

    pildoras - video 18 - panel de administracion III (configurar el panel de administrador)

--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 class Clientes(models.Model):
     nombre = models.CharField(max_length = 30)
-    direccion = models.CharField(max_length = 50)
+    direccion = models.CharField(max_length = 50, verbose_name = "La dirección")
     email = models.EmailField(blank = True, null = True)
     tfno = models.CharField(max_length = 7)
+
+    def __str__(self) -> str:
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length = 30)
