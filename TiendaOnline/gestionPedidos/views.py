1435e45a51f3b5c3b7bb8c0c8e2aa54cdafa42c5
danielorrus <drgponte@gmail.com>
2022-03-04 17:57:31 +0100

    pildoras - video 21 - formularios I (crear formulario que devuelve textbox al pulsar boton)

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+    mensaje = "Artículo buscado: %r" %request.GET["prd"]
+    return HttpResponse("<h1>" + mensaje + "</h1>")
